35f13d422e8fafa1f6297377e899ebd537bba8cb
Erlend Hamberg <ehamberg@gmail.com>
2012-08-04 14:20:16 +0200

    generate identifier

--- Handler/New.hs
+++ Handler/New.hs
@@ -2,8 +2,18 @@
 module Handler.New where
 
 import Import
+import System.Random
+import Data.Char
+import Data.Text (pack)
+
+createIdentifier :: Int -> IO Text
+createIdentifier len = do
+    g <- getStdGen
+    let str = take len . filter isAlphaNum . map chr $ randomRs (ord '0', ord 'z') g
+    return $ pack str
 
 getNewR :: Handler RepHtml
 getNewR = do
-    feedId <- runDB $ insert $ Feed "My RSS Queue"
+    ident <- liftIO $ createIdentifier 6
+    feedId <- runDB $ insert $ Feed "My RSS Queue" ident
     redirect (FeedR feedId)
--- Model.hs
+++ Model.hs
@@ -5,12 +5,6 @@ import Yesod
 import Data.Text (Text)
 import Database.Persist.Quasi
 import Data.Time
-import Data.Int
 
-
--- You can define all of your database entities in the entities file.
--- You can find more information on persistent and how to declare entities
--- at:
--- http://www.yesodweb.com/book/persistent/
 share [mkPersist sqlSettings, mkMigrate "migrateAll"]
     $(persistFileWith lowerCaseSettings "config/models")
