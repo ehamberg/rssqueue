ca8f47dbee969c3f43f04e51386ca58bcbb62cf9
Erlend Hamberg <ehamberg@gmail.com>
2012-08-05 21:33:54 +0200

    make “new” page ask for a feed name

--- Handler/New.hs
+++ Handler/New.hs
@@ -3,9 +3,32 @@ module Handler.New where
 
 import Import
 
+newtype NewFeedTitle = NewFeedTitle Text deriving Show
+
+feedTitleForm :: Html -> MForm App App (FormResult NewFeedTitle, Widget)
+feedTitleForm = renderDivs $ NewFeedTitle
+    <$> areq textField "Name" Nothing
+
 getNewR :: Handler RepHtml
 getNewR = do
-    -- create a six-character, random identifier for the new feed
-    ident <- liftIO $ createIdentifier 6
-    _ <- runDB $ insert $ Feed ident "My RSS Queue"
-    redirect (FeedR ident)
+    (widget, enctype) <- generateFormPost feedTitleForm
+    defaultLayout [whamlet|
+        <h2>New Feed
+        <form method=post action=@{NewR} enctype=#{enctype}>
+            ^{widget}
+            <input type=submit value="Continue →">
+    |]
+
+postNewR :: Handler RepHtml
+postNewR = do
+    ((result, _), _) <- runFormPost feedTitleForm
+
+    case result of
+         FormSuccess (NewFeedTitle title) -> do
+            -- create a six-character, random identifier for the new feed
+            ident <- liftIO $ createIdentifier 6
+            -- insert into database and redirect to feed page
+            _ <- runDB $ insert $ Feed ident title
+            redirect (FeedR ident)
+         -- on errors, simply redirect to “new”
+         _ -> redirect (NewR)
