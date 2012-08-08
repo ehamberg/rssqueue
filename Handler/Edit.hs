bb9c35b32e624d5472ac7b668a6b1a522ede7279
Erlend Hamberg <ehamberg@gmail.com>
2012-08-08 15:48:25 +0200

    change hard-coded form to yesod form

--- Handler/Edit.hs
+++ Handler/Edit.hs
@@ -6,17 +6,23 @@ module Handler.Edit where
 import Import
 import Data.Time.Clock (getCurrentTime)
 import Network.Wai (remoteHost)
-import Data.Maybe (fromMaybe)
 import Text.Julius (ToJavascript, toJavascript)
 
 instance ToJavascript Identifier where
     toJavascript (Identifier t) = toJavascript t
+
+addItemForm :: Html -> MForm RSSQueueApp RSSQueueApp (FormResult (Text,Text), Widget)
+addItemForm = renderDivs $ (,)
+    <$> areq textField "Title" Nothing
+    <*> areq textField "URL" Nothing
 
 getEditR :: Identifier -> Handler RepHtml
 getEditR identifier = do
     Entity key queue <- runDB $ getBy404 $ UniqueIdentifier identifier
 
     items <- runDB $ selectList [QueueItemQueueId ==. key] [Desc QueueItemCreated] >>= mapM (\(Entity _ v) -> return v)
+    (widget, enctype) <- generateFormPost addItemForm
+
     defaultLayout $ do
         addScriptRemote "http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"
         addButtonId <- lift newIdent
@@ -29,10 +35,12 @@ postEditR :: Identifier -> Handler RepJson
 postEditR identifier = do
     Just (Entity key _) <- runDB $ getBy $ UniqueIdentifier identifier
 
-    title <- fmap (fromMaybe "") $ lookupPostParam "new_title"
-    url   <- fmap (fromMaybe "") $ lookupPostParam "new_url"
-    time <- liftIO getCurrentTime
-    ip <- fmap (getIpAddr . remoteHost . reqWaiRequest) getRequest
+    ((result, _), _) <- runFormPost addItemForm
 
-    _ <- runDB $ insert $ QueueItem key title url time ip
-    jsonToRepJson $ String "success"
+    case result of
+         FormSuccess (title,url) -> do
+             time <- liftIO getCurrentTime
+             ip <- fmap (getIpAddr . remoteHost . reqWaiRequest) getRequest
+             _ <- runDB $ insert $ QueueItem key title url time ip
+             jsonToRepJson $ String "success"
+         _ -> undefined -- error
