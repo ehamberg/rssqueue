048cb58110b7bf07ba929eab9d18afd1f29e3da5
Erlend Hamberg <ehamberg@gmail.com>
2012-08-08 21:48:42 +0200

    improve “edit” page
    - (sort of) validate url
    - report errors (for now just an alert)
    - prepend “http://” if needed

--- Handler/Edit.hs
+++ Handler/Edit.hs
@@ -3,18 +3,28 @@
 
 module Handler.Edit where
 
-import Import
+import Import hiding (length)
 import Data.Time.Clock (getCurrentTime)
 import Network.Wai (remoteHost)
 import Text.Julius (ToJavascript, toJavascript)
+import Data.Char (isAlphaNum)
+import Data.Maybe (isJust)
+import Data.Text (isPrefixOf, append, length, head, find)
 
 instance ToJavascript Identifier where
     toJavascript (Identifier t) = toJavascript t
 
 addItemForm :: Html -> MForm RSSQueueApp RSSQueueApp (FormResult (Text,Text), Widget)
 addItemForm = renderDivs $ (,)
-    <$> areq textField "Title" Nothing
-    <*> areq textField "URL" Nothing
+    <$> areq textField (FieldSettings "Title" Nothing (Just "item_title") Nothing []) Nothing
+    <*> areq urlField' (FieldSettings "URL"   Nothing (Just "item_url")   Nothing []) Nothing
+    where
+        urlField' = check validateURL textField
+        looksLikeUrl u =
+            length u >= 4 && isAlphaNum (head u) && isJust (find (=='.') u)
+        validateURL u
+          | looksLikeUrl u = Right u
+          | otherwise      = Left ("That doesn't look like an URL." :: Text)
 
 getEditR :: Identifier -> Handler RepHtml
 getEditR identifier = do
@@ -39,8 +49,14 @@ postEditR identifier = do
 
     case result of
          FormSuccess (title,url) -> do
+             let url' = if "http://" `isPrefixOf` url || "https://" `isPrefixOf` url
+                           then url
+                           else "http://" `append` url
              time <- liftIO getCurrentTime
              ip <- fmap (getIpAddr . remoteHost . reqWaiRequest) getRequest
-             _ <- runDB $ insert $ QueueItem key title url time ip
+             _ <- runDB $ insert $ QueueItem key title url' time ip
              jsonToRepJson $ String "success"
-         _ -> undefined -- error
+         FormFailure errors -> do
+             liftIO $ print errors
+             jsonToRepJson $ String $ mconcat errors
+         _ -> jsonToRepJson $ String "error"
