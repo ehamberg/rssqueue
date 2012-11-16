9debe35174ddc3bf2bebe965907106c50c5b9f7b
Erlend Hamberg <ehamberg@gmail.com>
2012-11-16 14:16:30 +0100

    add skeleton for help page

--- Application.hs
+++ Application.hs
@@ -23,6 +23,7 @@ import Handler.Edit
 import Handler.View
 import Handler.Feed
 import Handler.About
+import Handler.Help
 
 -- This line actually creates our YesodSite instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see
---
+++ Handler/Help.hs
@@ -0,0 +1,8 @@
+module Handler.Help where
+
+import Import
+
+getHelpR :: Handler RepHtml
+getHelpR = defaultLayout $ do
+  setTitle $ "Help – RSSQueue"
+  $(widgetFile "help")
