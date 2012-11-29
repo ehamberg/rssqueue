d022837a544c23efff8032f9141b4691368c32ba
Erlend Hamberg <ehamberg@gmail.com>
2012-11-29 12:59:16 +0100

    only use “hero unit” on tablet/desktop
    split welcome message into its own widget and choose to display it in a
    hero unit or not based on the device.

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -5,6 +5,7 @@ import Import
 
 getHomeR :: Handler RepHtml
 getHomeR = defaultLayout $ do
+    let welcomeWidget = $(widgetFile "welcome")
     aDomId <- lift newIdent
     setTitle "RSS Queue"
     $(widgetFile "homepage")
